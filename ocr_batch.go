2435dfaaf19e6bf8c33d8374cc485b5d9065fb02
hekmon <edouardhur@gmail.com>
2025-05-14 10:44:45 +0200

    batch WIP

--- ocr_batch.go
+++ ocr_batch.go
@@ -16,16 +16,78 @@ const (
 )
 
 func OCRBatched(ctx context.Context, imgSubs []PGSSubtitle, client openai.Client, model string, italic, debug bool) (txtSubs SRTSubtitles, err error) {
-	var line string
+	// Create the batches
+	var (
+		line string
+		size int
+	)
+	batches := make([][]string, 0, len(imgSubs)/batchMaxRequests+1)
+	currentBatch := make([]string, 0, min(batchMaxRequests, len(imgSubs)))
 	for id, sub := range imgSubs {
 		if line, err = batchCreateLine(id, model, sub.Image, italic); err != nil {
 			err = fmt.Errorf("failed to create batch line #%d: %w", id, err)
 			return
 		}
-		if debug {
-			fmt.Println(line)
+		size = batchSize(currentBatch, line)
+		if size > batchMaxSize || len(currentBatch) > batchMaxRequests {
+			if debug {
+				fmt.Printf("Creating a new batch. Current batch has %d requests for total size of %d", len(currentBatch), size)
+			}
+			batches = append(batches, currentBatch)
+			currentBatch = make([]string, 0, min(batchMaxRequests, len(imgSubs)-id))
 		}
+		currentBatch = append(currentBatch, line)
 	}
+	batches = append(batches, currentBatch)
+	if debug {
+		fmt.Printf("Last batch has %d requests for total size of %d", len(currentBatch), size)
+	}
+	// Update the files
+	var uploadedFile *openai.FileObject
+	uploadedFiles := make([]*openai.FileObject, 0, len(batches))
+	defer func() {
+		for i, f := range uploadedFiles {
+			res, deleteErr := client.Files.Delete(context.TODO(), f.ID)
+			if deleteErr != nil {
+				fmt.Printf("Failed to delete file %s (batch #%d) after processing: %v\n", f.ID, i, deleteErr)
+				continue
+			}
+			if res.Deleted != true {
+				fmt.Printf("File %s (batch #%d) was not deleted after processing\n", f.ID, i)
+				continue
+			}
+			if debug {
+				fmt.Printf("Successfully deleted file %s (batch #%d)\n", f.ID, i)
+			}
+		}
+	}()
+	for i, batch := range batches {
+		// TODO marshall
+		if uploadedFile, err = client.Files.New(ctx, openai.FileNewParams{
+			File:    nil,
+			Purpose: openai.FilePurposeBatch,
+		}); err != nil {
+			err = fmt.Errorf("failed to upload file for batch %d: %w", i, err)
+			return
+		}
+		uploadedFiles = append(uploadedFiles, uploadedFile)
+	}
+	fmt.Println(len(batches), "batches")
+	return
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func batchSize(currentBatch []string, newLine string) (totalSize int) {
+	for _, item := range currentBatch {
+		totalSize += len(item) + 1 // +1 for the newline character that will be added later in the JSON array representation.
+	}
+	totalSize += len(newLine) + 1
 	return
 }
 
